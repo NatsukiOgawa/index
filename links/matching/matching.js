2017e53c5129c8dc8f53a5f645f1248445c137ba
NatsukiOgawa <42310040+NatsukiOgawa@users.noreply.github.com>
2019-03-11 00:10:35 +0900

    Add files via upload

---
+++ links/matching/matching.js
@@ -0,0 +1,18 @@
+function searchLocation(idname){
+  // value値を取得する
+  var result = document.getElementById(idname).value;
+
+  // 最終的な処理を記述する
+  alert("テキストボックス内部の文字列は「" + result + "」　です。");
+
+}
+
+
+function loginId(idname){
+  // value値を取得する
+  var result = document.getElementById(idname).value;
+
+  // 最終的な処理を記述する
+  alert("テキストボックス内部の文字列は「" + result + "」　です。");
+
+}
